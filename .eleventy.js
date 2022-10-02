f832d3c0c57e84c671fd7be0ee087db4028c5415
selfire1 <try2banonym@gmail.com>
2022-10-03 07:58:09 +1000

    init

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/js/");
+    eleventyConfig.addPassthroughCopy({ "./node_modules/@doist/todoist-api-typescript/dist/TodoistApi.js": "/js/TodoistApi.js" });
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: '_includes',
+            layouts: 'layouts',
+            data: '_data'
+        },
+    };
+};
