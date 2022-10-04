39ef6ced5ca758e15c508b82955472b4eec5d017
selfire1 <try2banonym@gmail.com>
2022-10-04 13:27:54 +1000

    add serverless plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+    const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+    eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+        name: "serverless",
+        functionsDir: "./netlify/functions/"
+    });
     eleventyConfig.addPassthroughCopy("src/css/");
     eleventyConfig.addPassthroughCopy("src/js/");
     eleventyConfig.addPassthroughCopy({ "./node_modules/@doist/todoist-api-typescript/dist/TodoistApi.js": "/js/TodoistApi.js" });
