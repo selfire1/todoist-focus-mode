1cf8277628c95cbaeec61fe65c8b7be59698e3a8
selfire1 <try2banonym@gmail.com>
2022-11-18 13:30:08 +1000

    format with eslint

--- netlify/functions/auth-before.js
+++ netlify/functions/auth-before.js
@@ -1,23 +1,23 @@
-const { OAuth, getCookie, generateCsrfToken } = require("./util/auth.js");
+/* eslint-disable import/extensions */
+const { OAuth, getCookie, generateCsrfToken } = require('./util/auth.js');
 const providers = require('./util/providers.js');
 
 /* Do initial auth redirect */
-exports.handler = async (event, context) => {
-
+exports.handler = async (event) => {
   if (!event.queryStringParameters) {
     return {
       statusCode: 401,
       body: JSON.stringify({
         error: 'No token found',
-      })
-    }
+      }),
+    };
   }
 
   const csrfToken = generateCsrfToken();
-  const provider = event.queryStringParameters.provider;
+  const { provider } = event.queryStringParameters;
 
-  let oauth = new OAuth(provider);
-  let config = oauth.config;
+  const oauth = new OAuth(provider);
+  const { config } = oauth;
 
   const redirectUrl = (new URL(event.queryStringParameters.securePath, config.secureHost)).toString();
 
@@ -30,16 +30,16 @@ exports.handler = async (event, context) => {
     state: `url=${redirectUrl}&csrf=${csrfToken}&provider=${provider}`,
   });
 
-  console.log("[auth-start] SETTING COOKIE");
+  console.log('[auth-start] SETTING COOKIE');
 
   /* Redirect user to authorizationURI */
   return {
     statusCode: 302,
     headers: {
-      'Set-Cookie': getCookie("_11ty_oauth_csrf", csrfToken, 60 * 2), // 2 minutes
+      'Set-Cookie': getCookie('_11ty_oauth_csrf', csrfToken, 60 * 2), // 2 minutes
       Location: authorizationURI,
-      'Cache-Control': 'no-cache' // Disable caching of this response
+      'Cache-Control': 'no-cache', // Disable caching of this response
     },
-    body: '' // return body for local dev
-  }
-}
+    body: '', // return body for local dev
+  };
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
+/* eslint-disable indent */
+/* eslint-disable no-use-before-define */
 // Variables and page setup
 const infoElement = document.getElementById('token');
 const token = infoElement.value;
-const params = JSON.parse(infoElement.dataset.params);
 const projects = JSON.parse(infoElement.dataset.projects);
-let filterQuery = params.filter;
 window.tasks = JSON.parse(infoElement.dataset.tasks);
 window.index = 0;
-renderTask()
+renderTask();
 console.log(projects);
 console.log(window.tasks);
 
@@ -14,44 +14,44 @@ console.log(window.tasks);
 function renderTask(i = window.index) {
     const div = document.getElementById('task-current');
     if (!window.tasks[i]) {
-        div.innerText = "No tasks in this filter at the moment."
-        return
+        div.innerText = 'No tasks in this filter at the moment.';
+        return;
     }
     const currentTask = window.tasks[i].content;
     div.innerHTML = mdToHtmlElement(currentTask);
 
     const fm = document.getElementById('task-project');
-    let project = findProject(window.tasks[i].project_id, projects);
+    const project = findProject(window.tasks[i].project_id, projects);
     fm.innerHTML = sanitizeHTML(project);
 }
 // Buttons
 const btnNext = document.getElementById('btn-next');
-btnNext.addEventListener('click', function () { btnCounter(window.tasks); })
+btnNext.addEventListener('click', () => { btnCounter(window.tasks); });
 
 const btnPrev = document.getElementById('btn-prev');
-btnPrev.addEventListener('click', function () { btnCounter(window.tasks, false); })
+btnPrev.addEventListener('click', () => { btnCounter(window.tasks, false); });
 
 const btnDone = document.getElementById('btn-done');
-btnDone.addEventListener('click', function () {
+btnDone.addEventListener('click', () => {
     taskDone(window.tasks[window.index].id, token);
-    notify("☑ Marked task as done")
-})
+    notify('☑ Marked task as done');
+});
 // Closing a task
 async function taskDone(taskId, apiKey) {
-    let url = `https://api.todoist.com/rest/v2/tasks/${taskId}/close`;
-    let response = await fetch(url, {
-        method: "POST",
+    const url = `https://api.todoist.com/rest/v2/tasks/${taskId}/close`;
+    const response = await fetch(url, {
+        method: 'POST',
         headers: {
-            Authorization: `Bearer ${apiKey}`
-        }
-    })
+            Authorization: `Bearer ${apiKey}`,
+        },
+    });
     if (response.ok) {
-        console.log("Completed task");
-        window.tasks.splice(window.index, 1)
-        window.index--
-        renderTask()
+        console.log('Completed task');
+        window.tasks.splice(window.index, 1);
+        window.index--;
+        renderTask();
     } else {
-        alert("HTTP-Error: " + response.status);
+        alert(`HTTP-Error: ${response.status}`);
     }
 }
 // Keypress navigation
@@ -62,33 +62,34 @@ document.onkeydown = function (e) {
         case 37:
             // Left -> Previous
             if (window.index <= 0) {
-                break
+                break;
             }
-            btnCounter(window.tasks, false)
+            btnCounter(window.tasks, false);
             const btnPrev = document.getElementById('btn-prev');
             btnPrev.classList.add('hover');
-            setTimeout(() => { btnPrev.classList.remove('hover') }, 100)
+            setTimeout(() => { btnPrev.classList.remove('hover'); }, 100);
             break;
         case 39:
             // Right -> Next
             if (window.index >= window.tasks.length - 1) {
                 break;
             }
-            btnCounter(window.tasks)
+            btnCounter(window.tasks);
             const btnNext = document.getElementById('btn-next');
             btnNext.classList.add('hover');
-            setTimeout(() => { btnNext.classList.remove('hover') }, 100)
+            setTimeout(() => { btnNext.classList.remove('hover'); }, 100);
+            break;
+        default:
             break;
     }
 };
 
-
 // Button Counter
 function btnCounter(tasks, increase = true) {
     if (increase) {
-        window.index++
+        window.index++;
     } else {
-        window.index--
+        window.index--;
     }
     renderTask();
     if (window.index <= 0) {
@@ -104,77 +105,75 @@ function btnCounter(tasks, increase = true) {
 }
 // Action buttons
 const btnCopy = document.getElementById('btn-copy-url');
-btnCopy.addEventListener('click', function () {
-    let url = document.location.href;
-    navigator.clipboard.writeText(url).then(function () {
+btnCopy.addEventListener('click', () => {
+    const url = document.location.href;
+    navigator.clipboard.writeText(url).then(() => {
         console.log('Copied!');
-        notify("📋 Copied link to clipboard");
-    }, function () {
-        notify("⚠️ Error! Please copy manually");
+        notify('📋 Copied link to clipboard');
+    }, () => {
+        notify('⚠️ Error! Please copy manually');
         console.log('Copy error');
     });
-
-})
+});
 
 const btnUrl = document.getElementById('btn-open-td');
-btnUrl.addEventListener('click', function () {
+btnUrl.addEventListener('click', () => {
     const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/));
     if (isMobile) {
         // On mobile, open into the app
-        let url = "todoist://task?id=" + window.tasks[window.index].id;
-        window.open(url, "todoist tab")
+        const url = `todoist://task?id=${window.tasks[window.index].id}`;
+        window.open(url, 'todoist tab');
     } else {
-        window.open(window.tasks[window.index].url, "todoist tab")
+        window.open(window.tasks[window.index].url, 'todoist tab');
     }
-})
+});
 function notify(str) {
     const notifyBanner = document.getElementById('notify');
     notifyBanner.innerText = str;
     unfade(notifyBanner);
     setTimeout(() => {
         fade(notifyBanner);
-    }, 3000)
-
+    }, 3000);
 }
 
 // Fade out element
 function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
+    let op = 1; // initial opacity
+    const timer = setInterval(() => {
         if (op <= 0.1) {
             clearInterval(timer);
             element.style.display = 'none';
         }
         element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        element.style.filter = `alpha(opacity=${op * 100})`;
         op -= op * 0.1;
     }, 50);
 }
 // Fade in element
 function unfade(element) {
-    var op = 0.1;  // initial opacity
+    let op = 0.1; // initial opacity
     element.style.display = 'block';
-    var timer = setInterval(function () {
+    const timer = setInterval(() => {
         if (op >= 1) {
             clearInterval(timer);
         }
         element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        element.style.filter = `alpha(opacity=${op * 100})`;
         op += op * 0.15;
     }, 10);
 }
 // Parse Todoist's Markdown
 function mdToHtmlElement(str) {
-    var md = window.markdownit({
-        typographer: true
+    const md = window.markdownit({
+        typographer: true,
     });
-    var result = md.render(str);
+    const result = md.render(str);
     return result;
 }
 
 function findProject(tasksProjectID, projects) {
     const isProjectId = (element) => element.id == tasksProjectID;
-    const projectIndex = projects.findIndex(isProjectId)
+    const projectIndex = projects.findIndex(isProjectId);
     return projects[projectIndex].name;
 }
 
@@ -185,7 +184,7 @@ function findProject(tasksProjectID, projects) {
  * @return {String} str  The sanitized string
  */
 function sanitizeHTML(str) {
-    var temp = document.createElement('div');
+    const temp = document.createElement('div');
     temp.textContent = str;
     return temp.innerHTML;
-};
+}
